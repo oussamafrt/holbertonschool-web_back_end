e2703d0c99f4d0c3e4748dcc5683a6cffb7b12aa
oussamafrt <oussama.ferhat01@gmail.com>
2024-11-27 22:08:39 +0100

    promise

---
+++ ES6_promise/9-try.js
@@ -0,0 +1,13 @@
+/*eslint-disable*/
+export default function guardrail(mathFunction) {
+    const queue = [];
+    try {
+      const result = mathFunction();
+      queue.push(result);
+    } catch (error) {
+      queue.push(error.message);
+    } finally {
+      queue.push('Guardrail was processed');
+    }
+    return queue;
+  }
