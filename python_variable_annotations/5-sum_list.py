088017d30a9572f81e6c1d5156ecc7909b8e3f63
oussamafrt <oussama.ferhat01@gmail.com>
2024-12-05 10:53:36 +0100

    task 5

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Shebang script"""
+
+
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """Function that take list as argument"""
+
+
+    return sum(input_list)
