923bb36130f76a52c77e949342160eb23b795f1b
oussamafrt <oussama.ferhat01@gmail.com>
2024-12-12 10:30:13 +0100

    fix tasks

--- pagination/3-hypermedia_del_pagination.py
+++ pagination/3-hypermedia_del_pagination.py
@@ -37,7 +37,7 @@ class Server:
 
     def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
         """Deletion-resilient pagination with hypermedia support."""
-        assert isinstance(index, int) and 0 <= index < len(self.indexed_dataset())
+        assert isinstance(index, int) and 0 <= index < len(self.dataset())
 
         data = []
         current_index = index
