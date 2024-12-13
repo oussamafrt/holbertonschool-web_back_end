718ccea9d6c3351f36eed1124bb59ffc267df4b6
oussamafrt <oussama.ferhat01@gmail.com>
2024-12-13 22:12:03 +0100

    initial commit

---
+++ NoSQL/8-all.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" List all documents in Python """
+
+from pymongo import MongoClient
+
+
+client = MongoClient("mongodb://localhost:27017/")
+db = client.mydatabase
+mongo_collection = db.school
+
+def list_all(mongo_collection):
+    """ lists all documents in a collection """
+    return [doc for doc in mongo_collection.find()]
