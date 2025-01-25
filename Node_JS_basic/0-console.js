30e26f99874b4093fdedc465f5d0bfbaf334121d
oussamafrt <oussama.ferhat01@gmail.com>
2025-01-25 23:14:45 +0100

    initial commit

---
+++ Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Function to display a message in the console.
+ *
+ * @param {string} message The message to display.
+ */
+function displayMessage(message) {
+    console.log(message);
+  }
+  
+  module.exports = displayMessage;
